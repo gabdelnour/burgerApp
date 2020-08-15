9293ec3828372f0b960c89aa7bd11cd4d446c7b4
Gustavo Abdelnour <gurabdelnour@gmail.com>
2020-08-15 13:03:28 -0400

    added more and less buttons

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,13 @@ import style from "./BuildControl.module.css";
 const BuildControl = (props) => (
   <div className={style.BuildControl}>
     <div className={style.Label}>{props.Label}</div>
-    <button className={style.Less}>Less</button>
+    <button
+      className={style.Less}
+      onClick={props.removed}
+      disabled={props.disabled}
+    >
+      Less
+    </button>
     <button className={style.More} onClick={props.added}>
       More
     </button>
